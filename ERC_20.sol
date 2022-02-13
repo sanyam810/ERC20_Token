e70e85028108c29a0e41bdaba7ffd005611c9843
Sanyam Saini <72260960+sanyam810@users.noreply.github.com>
2022-02-13 19:43:34 +0530

    Add files via upload
    With an ICO

--- ERC_20.sol
+++ ERC_20.sol
@@ -30,7 +30,7 @@ contract DEDSEC is ERC20Interface{
     mapping(address=>mapping(address=>uint)) allowed;
 
     constructor(){
-        totalsupply=10000;
+        totalsupply=100000;
         founder=msg.sender;
         balances[founder]=totalsupply;
     }
@@ -39,7 +39,7 @@ contract DEDSEC is ERC20Interface{
         return balances[tokenowner];
     }
 
-    function transfer(address to,uint tokens)public override returns(bool success){
+    function transfer(address to,uint tokens)public virtual override returns(bool success){
         require(balances[msg.sender]>=tokens);
 
         balances[to]+=tokens;
@@ -63,7 +63,7 @@ contract DEDSEC is ERC20Interface{
         return true;
     }
 
-    function transferfrom(address from,address to,uint tokens)public override returns(bool success){
+    function transferfrom(address from,address to,uint tokens)public virtual override returns(bool success){
         require(allowed[from][to]>=tokens);
         require(balances[from]>=tokens);
 
@@ -74,6 +74,106 @@ contract DEDSEC is ERC20Interface{
         emit Transfer(from,to,tokens);
 
         return true;
-    } 
+    }
 
 }
+
+contract DedSecICO is DEDSEC{
+
+        address public admin;
+        address payable public deposit;
+        uint tokenPrice=0.001 ether;
+        uint public hardCap=300 ether;
+        uint public raisedAmount;
+        uint public saleStart=block.timestamp;
+        uint public saleEnd=block.timestamp+604800;
+        uint public tokenTradeStart=saleEnd+604800;
+        uint public maxInvesetment=5 ether;
+        uint public minInvestment=0.1 ether;
+
+        enum state{beforeStart,running,afterEnd,halted}
+        state public icoState;
+    
+        constructor(address payable _deposit){
+            deposit=_deposit;
+            admin=msg.sender;
+            icoState=state.beforeStart;
+        }
+
+        modifier onlyadmin(){
+            require(msg.sender==admin);
+            _;
+        }
+
+        function halt()public onlyadmin{
+            icoState=state.halted;
+        }
+
+        function resume()public onlyadmin{
+            icoState=state.running;
+        }
+
+        function changeDepositAddress(address payable newDeposit)public onlyadmin{
+            deposit=newDeposit;
+        }
+
+        function getCurrentState()public view returns(state){
+            if(icoState==state.halted){
+                return state.halted;
+            }
+            else if(block.timestamp<saleStart){
+                return state.beforeStart;
+            }
+            else if(block.timestamp>=saleStart && block.timestamp<=saleEnd){
+                return state.running;
+            }
+            else{
+                return state.afterEnd;
+            }
+        }
+
+        event Invest(address investor,uint value,uint tokens);
+        
+        function invest()payable public returns(bool){
+            icoState=getCurrentState();
+            require(icoState==state.running);
+
+            require(msg.value>=minInvestment && msg.value<=maxInvesetment);
+            raisedAmount+=msg.value;
+            require(raisedAmount<=hardCap);
+
+            uint tokens=msg.value/tokenPrice;
+
+            balances[msg.sender]+=tokens;
+            balances[founder]-=tokens;
+            deposit.transfer(msg.value);
+            
+            emit Invest(msg.sender,msg.value,tokens);
+            return true;
+        }
+
+        receive()payable external{
+            invest();
+        }
+
+        function transfer(address to,uint tokens)public  override returns(bool success){
+            require(block.timestamp>tokenTradeStart);
+            DEDSEC.transfer(to,tokens);
+            return true;
+        }
+
+
+        function transferfrom(address from,address to,uint tokens)public override returns(bool success){
+            require(block.timestamp>tokenTradeStart);
+            DEDSEC.transferfrom(from,to,tokens);
+            return true;
+        }
+
+        function burn() public returns(bool){
+            icoState=getCurrentState();
+            require(icoState==state.afterEnd);
+            balances[founder]=0;
+            return true;
+        }
+
+    }
